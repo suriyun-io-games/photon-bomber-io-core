ebdfa912857fe0bf412bdf34fbf309bacc641566
Insthync <ittipon.bay@gmail.com>
2020-11-30 23:27:40 +0700

    Fix get wrong bots

--- Network/IONetworkGameRule.cs
+++ Network/IONetworkGameRule.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
 
 public class IONetworkGameRule : BaseNetworkGameRule
 {
@@ -69,5 +70,10 @@ public class IONetworkGameRule : BaseNetworkGameRule
             ui = Instantiate(uiGameplayPrefab);
         if (ui != null)
             ui.gameObject.SetActive(true);
+    }
+
+    protected override List<BaseNetworkGameCharacter> GetBots()
+    {
+        return new List<BaseNetworkGameCharacter>(FindObjectsOfType<BotEntity>());
     }
 }
